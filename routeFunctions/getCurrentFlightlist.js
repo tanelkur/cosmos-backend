03568277e4497d64e0c8ca19fae68c9b6f39b890
tanelkur <106887540+tanelkur@users.noreply.github.com>
2023-08-28 08:46:49 +0300

    Add files via upload

--- routeFunctions/getCurrentFlightlist.js
+++ routeFunctions/getCurrentFlightlist.js
@@ -4,9 +4,11 @@ const getCurrentFlightlist = async (res, FLIGHTS_API) => {
   try {
     const response = await axios.get(FLIGHTS_API);
     const { id, validUntil, legs } = response.data;
+    const flights = { id, validUntil, legs };
 
-    res.status(200).send({ id, validUntil, legs });
+    res.status(200).send(flights);
   } catch (error) {
+    console.log("Backend axios");
     console.log(error.message);
   }
 };
